d514132ae9ec4d80b74f3e4deda43c4c840e3b60
iamjinpark <wldls7754@gmail.com>
2024-02-26 04:33:09 +0900

    [test]

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,9 +3,18 @@ import router from './router';
 
 function App() {
   return (
-    <div className="max-w-screen-md mx-auto">
-      <RouterProvider router={router} />
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/mypage/account" element={<AccountManagement />} />
+        <Route path="/mypage/editProfile" element={<EditProfile />} />
+        <Route path="/mypage/album" element={<MyPage />} />
+        <Route path="/mypage/board" element={<MyPage />} />
+        <Route path="/mypage/bookmark" element={<MyPage />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 export default App;
