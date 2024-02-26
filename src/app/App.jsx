d19c16d999b8b5c8cba8f65daa7a9faecb72df67
iamjinpark <wldls7754@gmail.com>
2024-02-26 17:11:13 +0900

    [test]

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,20 +1,15 @@
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import MyPage from '@/pages/MyPage/MyPage';
+import AccountManagement from '@/pages/AccountManagement/AccountManagement';
+import Header from '@/molecules/Header/Header';
+import EditProfile from '@/pages/EditProfile/EditProfile';
+import Footer from '@/atoms/Footer/Footer';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/mypage/account" element={<AccountManagement />} />
-        <Route path="/mypage/editProfile" element={<EditProfile />} />
-        <Route path="/mypage/album" element={<MyPage />} />
-        <Route path="/mypage/board" element={<MyPage />} />
-        <Route path="/mypage/bookmark" element={<MyPage />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <div>
+      <h1>Hello</h1>
+    </div>
   );
 }
 export default App;
