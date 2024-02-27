80dc82f374aa46954a451efd63f9a8591c54e063
iamjinpark <wldls7754@gmail.com>
2024-02-27 16:02:43 +0900

    ✨ [feat] 랜딩-디테일페이지 카테고리 컴포넌트 구현

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,13 @@
 import CategoryButton from '../atoms/CategoryButton/CategoryButton.jsx';
 import Category from '@/molecules/Category/Category.jsx';
-import FileInput from '@/atoms/FileInput/FileInput.jsx';
-import ImageAddButton from '@/atoms/FileInput/FileInput.jsx';
 
 function App() {
-  return <CategoryButton />;
+  return (
+    <div>
+      <Category />
+      <FileInput />
+      <ImageAddButton />
+    </div>
+  );
 }
 export default App;
--- src/atoms/DetailCategory/DetailCategory.jsx
+++
@@ -1,9 +0,0 @@
-const DetailCategoryButton = ({ text = '버튼' }) => {
-  return (
-    <button className="w-[290px] h-[30px] text-gray-300 text-[15px] hover:text-[20px] hover:text-black hover:font-bold">
-      {text}
-    </button>
-  );
-};
-
-export default DetailCategoryButton;
