2f21a1c482327552e107221af2fd7bcc72532155
iamjinpark <wldls7754@gmail.com>
2024-02-27 15:00:01 +0900

    ✨ [feat] Category 컴포넌트 구현

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,15 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MyPage from '@/pages/MyPage/MyPage';
-import AccountManagement from '@/pages/AccountManagement/AccountManagement';
-import Header from '@/molecules/Header/Header';
-import EditProfile from '@/pages/EditProfile/EditProfile';
-import Footer from '@/atoms/Footer/Footer';
+import CategoryButton from '../atoms/CategoryButton/CategoryButton.jsx';
+import Category from '@/molecules/Category/Category.jsx';
+import FileInput from '@/atoms/FileInput/FileInput.jsx';
+import ImageAddButton from '@/atoms/FileInput/FileInput.jsx';
 
 function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
+  return <CategoryButton />;
 }
 export default App;
