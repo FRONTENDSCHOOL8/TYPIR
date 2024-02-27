9f9c03511a1774fc7a3a791001146fb6467453a5
Lulurem <smile71109@naver.com>
2024-02-28 01:52:27 +0900

    ✨ [feat] 페이지별 라우팅 구성 #5

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,7 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import MyPage from '@/pages/MyPage/MyPage';
-import AccountManagement from '@/pages/AccountManagement/AccountManagement';
-import Header from '@/molecules/Header/Header';
-import EditProfile from '@/pages/EditProfile/EditProfile';
-import Footer from '@/atoms/Footer/Footer';
+import CategoryButton from '../atoms/CategoryButton/CategoryButton.jsx';
+import Category from '@/molecules/Category/Category.jsx';
+import FileInput from '@/atoms/FileInput/FileInput.jsx';
+import ImageAddButton from '@/atoms/FileInput/FileInput.jsx';
 
 function App() {
   return (
