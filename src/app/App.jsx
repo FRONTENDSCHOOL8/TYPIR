f18083ede23f9c8810b58469ae0f203d34cc33e0
iamjinpark <wldls7754@gmail.com>
2024-02-27 15:00:01 +0900

    ✨ [feat] Category 컴포넌트 구현

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,46 +1,11 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Layout from '@/route/Layout';
-import Splash from '@/pages/Splash/Splash';
-
-/* 마이 페이지, 헤더 관련 */
-import MyPage from '@/pages/MyPage/MyPage';
-import AccountManagement from '@/pages/AccountManagement/AccountManagement';
-import EditProfile from '@/pages/EditProfile/EditProfile';
-import NewStyle from '@/pages/NewStyle/NewStyle';
-import NewBoard from '@/pages/NewBoard/NewBoard';
-import SelectPostImage from '@/pages/SelectPostImage/SelectPostImage';
-import MyImageTemplate from '@/molecules/MyImageTemplate/MyImageTemplate';
-import MyDetailImage from '@/molecules/MyDetailImage/MyDetailImage';
-import BoardTemplate from '@/molecules/BoardTemplate/BoardTemplate';
-import MyPostTemplate from '@/molecules/MyPostTemplate/MyPostTemplate';
-
-/* 랜딩 페이지 관련 */
-import Landing from '@/pages/Landing/Landing';
-import DetailImage from '@/molecules/DetailImage/DetailImage';
-
-/* 커뮤니티 페이지 관련 */
-import Community from '@/pages/Community/Community';
-import CommunityDetail from '@/pages/CommunityDetail/CommunityDetail';
-
-/* 유저 페이지 관련 */
-import SignIn from '@/pages/SignIn/SignIn';
-import SignUp from '@/pages/SignUp/SignUp';
-import SetInitialProfile from '@/pages/SetInitialProfile/SetInitialProfile';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/mypage/account" element={<AccountManagement />} />
-        <Route path="/mypage/editProfile" element={<EditProfile />} />
-        <Route path="/mypage/album" element={<MyPage />} />
-        <Route path="/mypage/board" element={<MyPage />} />
-        <Route path="/mypage/bookmark" element={<MyPage />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <div>
+      <h1>Hello</h1>
+    </div>
   );
 }
 export default App;
