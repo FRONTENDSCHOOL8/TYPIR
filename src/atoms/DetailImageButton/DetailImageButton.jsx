caa21bc2736266304df1cb6f1a6639a75e67dc01
iamjinpark <wldls7754@gmail.com>
2024-02-28 17:05:58 +0900

    ✨ [feat] 카테고리 디테일 페이지 컴포넌트 구현

---
+++ src/atoms/DetailImageButton/DetailImageButton.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+function HandleModal() {
+  const [showModal, setShowModal] = useState('');
+  const openModal = () => {
+    setShowModal(true);
+  };
+}
+
+const DetailImageButton = () => {
+  return (
+    <button className="w-[48px] h-[48px] rounded-full bg-black flex justify-center items-center">
+      <svg height="20" width="20" viewBox="0 0 24 24" aria-hidden="true" aria-label="" role="img" className="m-auto">
+        <path
+          fill="white"
+          d="M12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6M3 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6m18 0a3 3 0 1 0 0 6 3 3 0 0 0 0-6"
+        ></path>
+      </svg>
+    </button>
+  );
+};
+
+export default DetailImageButton;
