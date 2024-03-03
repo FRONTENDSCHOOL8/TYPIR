b85405b936f3ad7739dd12e538f94e2845598f1e
iamjinpark <wldls7754@gmail.com>
2024-03-04 03:14:33 +0900

    ✨ [feat] masonry 템플릿에 이미지 렌더링 구현 완료

--- src/molecules/Category/CategoryNew.jsx
+++ src/molecules/Category/CategoryNew.jsx
@@ -2,60 +2,13 @@ import { useState, useEffect } from 'react';
 import { NavLink, useSearchParams, useLocation } from 'react-router-dom';
 import { getPbImage } from '@/utils';
 import pb from '@/api/pocketbase';
+import ImageTemplate from '../ImageTemplate/ImageTemplate';
 
 const CATEGORIES = ['all', 'simple', 'daily', 'vintage'];
 
 function getPbImageURL(item, fileName = 'images') {
   return `${import.meta.env.VITE_PB_URL}/api/files/${item.collectionId}/${item.id}/${item[fileName]}`;
 }
-
-// async function getData() {
-//   const data = [
-//     {
-//       id: 'item-1',
-//       content: 'all content',
-//       category: 'all',
-//     },
-//     {
-//       id: 'item-2',
-//       content: 'vintage content',
-//       category: 'vintage',
-//     },
-//     {
-//       id: 'item-3',
-//       content: 'vintage content',
-//       category: 'vintage',
-//     },
-//     {
-//       id: 'item-4',
-//       content: 'simple content',
-//       category: 'simple',
-//     },
-//     {
-//       id: 'item-5',
-//       content: 'vintage content',
-//       category: 'vintage',
-//     },
-//     {
-//       id: 'item-6',
-//       content: 'simple content',
-//       category: 'simple',
-//     },
-//   ];
-//   return data;
-
-//   // const styles = await pb.collection('styles').getFullList();
-//   // const stylesImages = styles.map((style) => {
-//   //   const imageURL = getPbImage(style);
-//   //   style.image = imageURL;
-//   //   return style;
-//   // });
-
-//   // return {
-//   //   ...styles,
-//   //   image: stylesImages,
-//   // };
-// }
 
 async function getData() {
   const styles = await pb.collection('styles').getFullList();
@@ -64,45 +17,9 @@ async function getData() {
     return { ...style, image: imageURL };
   });
 
-  console.log(stylesWithImages);
-  console.log(getPbImageURL(styles));
+  // console.log(stylesWithImages);
   return stylesWithImages;
 }
-
-// const Category = ({ gap = 'gap-3' }) => {
-//   const [data, setData] = useState(null);
-//   const [searchParams] = useSearchParams();
-//   const selectedCategory = searchParams.get('category');
-//   useEffect(() => {
-//     getData().then(setData);
-//   }, []);
-//   if (!data) {
-//     return <p>로딩 중...</p>;
-//   }
-//   const filteredCategoryData =
-//     selectedCategory === 'all' ? data : data.filter((item) => item.category === selectedCategory);
-//   return (
-//     <div className="mt-[5px] mb-[15px] w-full">
-//       <ul className={`flex flex-row ${gap} font-serif`}>
-//         {CATEGORIES.map((category) => (
-//           <li
-//             key={category}
-//             className={`cursor-pointer px-[0.5625rem] border border-gray-200 rounded-xl ${selectedCategory === category ? 'bg-black' : 'bg-white'} ${selectedCategory === category ? 'text-white' : 'text-gray-200'}`}
-//           >
-//             <NavLink to={`/category?category=${category}`}>{category[0].toUpperCase() + category.slice(1)}</NavLink>
-//           </li>
-//         ))}
-//       </ul>
-//       <ul>
-//         {filteredCategoryData.map((categoryItem) => (
-//           <li key={categoryItem.id}>{categoryItem.content}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default Category;
 
 const Category = ({ gap = 'gap-3' }) => {
   const [data, setData] = useState(null);
@@ -132,7 +49,8 @@ const Category = ({ gap = 'gap-3' }) => {
           </li>
         ))}
       </ul>
-      <ul>
+      <ImageTemplate data={filteredCategoryData} />
+      {/* <ul>
         {filteredCategoryData.map((categoryItem) => (
           <li key={categoryItem.id}>
             <div>
@@ -141,7 +59,7 @@ const Category = ({ gap = 'gap-3' }) => {
             </div>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 };
--- src/molecules/ImageTemplate/ImageTemplate.jsx
+++ src/molecules/ImageTemplate/ImageTemplate.jsx
@@ -1,7 +1,7 @@
 import Masonry from 'react-masonry-css';
 import images from '/src/data/images.json';
 
-function ImageTemplate() {
+function ImageTemplate({ data = images }) {
   const breakpointColumnsObj = {
     default: 4,
     650: 3,
@@ -9,23 +9,24 @@ function ImageTemplate() {
   };
 
   return (
-    <div className="max-w-screen-md h-auto bg-white">
+    <ul className="max-w-screen-md h-auto mt-[15px]">
       <Masonry
         breakpointCols={breakpointColumnsObj}
         className="my-masonry-grid flex gap-[12px]"
         columnClassName="my-masonry-grid_column flex flex-col items-center"
       >
-        {images.map((item) => (
-          <div
-            key={item.id}
-            className={`w-[170px] bg-gray-100 rounded-2xl mb-[15px]`}
-            style={{ height: `${item.height}px` }}
-          >
-            {item.content}
-          </div>
+        {data.map((item) => (
+          <li key={item.id}>
+            <img
+              src={item.image}
+              alt={item.alt}
+              className={`w-[170px] bg-gray-100 rounded-2xl mb-[15px]`}
+              style={{ height: `${item.height}px` }}
+            />
+          </li>
         ))}
       </Masonry>
-    </div>
+    </ul>
   );
 }
 export default ImageTemplate;
