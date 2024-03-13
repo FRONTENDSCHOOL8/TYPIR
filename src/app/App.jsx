38c4e90a8c674c316f8d616c0240fb9c5ffbd515
yeseul0000 <fiddich999@gmail.com>
2024-03-13 15:23:15 +0900

    ⚡️ [feat] privateRoute 기능 추가

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from '@/route/Layout';
+import Splash from '@/pages/Splash/Splash';
+
+/* 마이 페이지, 헤더 관련 */
 import MyPage from '@/pages/MyPage/MyPage';
 import AccountManagement from '@/pages/AccountManagement/AccountManagement';
-import Header from '@/molecules/Header/Header';
 import EditProfile from '@/pages/EditProfile/EditProfile';
 import Footer from '@/atoms/Footer/Footer';
+import MyPageRoutes from '@/Routes/MyPageRoutes';
+import CommunityPageRoutes from '@/Routes/CommunityPageRoutes';
+import UserPageRoutes from '@/Routes/UserPageRoutes';
+import LandingPageRoutes from '@/Routes/LandingPageRoutes';
 
 function App() {
   return (
